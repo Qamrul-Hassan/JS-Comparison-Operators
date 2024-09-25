94956f1f3c1d6de143358905b3a248d15a3ec563
Qamrul-Hassan <your-email@example.com>
2024-09-25 22:11:35 +0600

    massage

---
+++ Q6.js
@@ -0,0 +1,8 @@
+//6. Is the temperature below freezing (0°C)?
+const tempreture = -5;
+if (tempreture < 0) {
+    console.log("It is below freezing");
+
+}else {
+    console.log("Its above freezing.");
+}
