91f6f229b6159788a1467b49bc70d07aed1ee3bd
Qamrul-Hassan <your-email@example.com>
2024-09-25 22:08:28 +0600

    massage

---
+++ Q5.js
@@ -0,0 +1,7 @@
+//5. Is the number less than or equal to 50?
+const number =45;
+if (number <= 50) {
+    console.log("The number is less than or equal to 50.");
+} else {
+    console.log("The number is greater than 50.");
+}
